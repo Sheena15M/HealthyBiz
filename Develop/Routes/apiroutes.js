d6629c6ea721ffd74f18d5cbbbd44d19443ac4ab
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-10-25 00:10:20 -0400

    more routes

--- Develop/Routes/apiroutes.js
+++ Develop/Routes/apiroutes.js
@@ -1,5 +1,6 @@
 const Workout = require('../models /workout')
 
+//If the user wanted to find their past workouts, thhis is how it's searched for
 module.exports = function(app){
     app.get('/api/workouts',function(req,res){
         Workout.find()
@@ -10,9 +11,9 @@ module.exports = function(app){
             res.json(err)
         })
     });
-    
+
     app.post('/api/workouts', function(req,res){
-        Workout.find()
+        Workout.create()
         .then(data =>{
             res.json(data)
         })
@@ -30,4 +31,19 @@ module.exports = function(app){
             res.json(err)
         })
     });
+
+    app.post('/api/workouts/range', function (req,res){
+        Workout.create({})
+        .then(data => res.json(data))
+        .catch(err => {
+            res.json(err)
+        })
+    });
+
+    app.put('/api/workouts/:id', ({body,params},res)=>{
+        Workout.findByIdAndUpdate(
+            params.id,
+            {$push:{exercises:body} },
+        )
+    })
 }
