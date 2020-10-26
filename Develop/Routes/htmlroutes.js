7e8af852415b228cad46667793552bbbbfa66e1d
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-10-26 12:05:02 -0400

    code added to html routes

--- Develop/Routes/htmlroutes.js
+++ Develop/Routes/htmlroutes.js
@@ -5,7 +5,11 @@ const path = require("path")
 router.get("/exercise", (req, res)=> {
     console.log("hree")
     res.sendFile(path.join(__dirname, "../public/exercise.html"))
-})
+});
+
+applicationCache.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,'../public/index.html'));
+});
 
 
 module.exports = router; 
