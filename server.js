a41638c4095cfc2217fde22904599bb9282352bd
TASHENA MALLOY <SHEENA15M@yahoo.com>
2020-10-29 22:15:29 -0400

    change routes

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 //Route to the API and make sure your port knows to listen out
 // app.use(require(".api.js"));
 
-const apiRoutes = require("./Routes/apiRoutes.js")
+const apiRoutes = require("./Routes/apiRoutes")
 apiRoutes(app)
-app.use(require("./Routes/htmlRoutes.js"))
+app.use(require("./Routes/htmlRoutes"))
 
 
 
